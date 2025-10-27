e41b2f97ae9580f9da8b27b845cce03c00e362f2
Constantin Chaumet <16308584+Jekannadar@users.noreply.github.com>
2025-10-27 13:13:06 +0100

    Add test for 'Component' class

---
+++ tests/test_component.py
@@ -0,0 +1,28 @@
+# test for pseudo-frozen dataclass Component working correctly
+from dataclasses import FrozenInstanceError
+from inspect import signature
+
+import pytest
+from cosy import Component
+
+
+@pytest.fixture
+def component_no_arguments():
+    return Component("", lambda: "")
+
+
+@pytest.fixture
+def component_with_five_arguments():
+    return Component("", lambda _, __, ___, ____, _____: "")
+
+
+def test_component_frozen(component_no_arguments) -> None:
+    with pytest.raises(FrozenInstanceError):
+        component_no_arguments.interpretation = lambda: ""
+    with pytest.raises(FrozenInstanceError):
+        component_no_arguments.name = ""
+
+
+def test_wrapper_updated(component_no_arguments, component_with_five_arguments) -> None:
+    assert len(list(signature(component_no_arguments).parameters.values())) == 0
+    assert len(list(signature(component_with_five_arguments).parameters.values())) == 5
