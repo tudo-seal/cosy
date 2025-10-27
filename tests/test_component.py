bb39d06eeb07d03477f36fa3a22e6593dca783b9
Constantin Chaumet <16308584+Jekannadar@users.noreply.github.com>
2025-10-27 13:19:11 +0100

    Update test for 'Component' class to ensure it is wrapper as intended

--- tests/test_component.py
+++ tests/test_component.py
@@ -8,12 +8,12 @@ from cosy import Component
 
 @pytest.fixture
 def component_no_arguments():
-    return Component("", lambda: "")
+    return Component("", lambda: "0")
 
 
 @pytest.fixture
 def component_with_five_arguments():
-    return Component("", lambda _, __, ___, ____, _____: "")
+    return Component("", lambda _, __, ___, ____, _____: "5")
 
 
 def test_component_frozen(component_no_arguments) -> None:
@@ -26,3 +26,8 @@ def test_component_frozen(component_no_arguments) -> None:
 def test_wrapper_updated(component_no_arguments, component_with_five_arguments) -> None:
     assert len(list(signature(component_no_arguments).parameters.values())) == 0
     assert len(list(signature(component_with_five_arguments).parameters.values())) == 5
+
+
+def test_call_components(component_no_arguments, component_with_five_arguments) -> None:
+    assert component_no_arguments() == "0"
+    assert component_with_five_arguments(None, None, None, None, None) == "5"
